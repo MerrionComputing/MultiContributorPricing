075ad7bd6c8fee8297002b4704a9480c3dfe89db
duncan.jones <duncan.jones@DESKTOP-19FDK0S>
2020-02-03 17:29:30 +0000

    Outline class for PriceAmount

---
+++ src/MultiContributorPricing_FunctionApp/MultiContributorPricing_FunctionApp/Price/PriceAmount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiContributorPricing_FunctionApp.Price
+{
+    public class PriceAmount
+    {
+
+        /// <summary>
+        /// ISO 3 letter currency code in which the priced item is priced
+        /// </summary>
+        string CurrencyCode { get; set; }
+
+        /// <summary>
+        /// The amnount in the priced currency
+        /// </summary>
+        decimal Amount { get; set; }
+
+    }
+}
